d25f053a78272459316f7209d5f3a8a8e2abc6cc
Alberto Medina <albertomedina@google.com>
2017-06-14 14:59:38 -0700

    Add draft of test cases (not done\!)

--- includes/options/views/class-amp-analytics-options-serializer.php
+++ includes/options/views/class-amp-analytics-options-serializer.php
@@ -5,7 +5,7 @@ class Analytics_Options_Serializer {
 	public function init() {
 		add_action( 'admin_post_analytics_options', array( $this, 'save' ) );
 	}
-	
+
 	public static function save() {
 
 		$option_name = 'analytics';
@@ -22,18 +22,17 @@ class Analytics_Options_Serializer {
 			stripslashes($_POST['config'])
 		);
 
-		$inner_option_name = $_POST['vendor-type'] . '-' . $_POST['id'];
-
 		$analytics_options = get_option($option_name);
 		if ( ! $analytics_options ) {
 			$analytics_options = array();
-		}
 
+		$inner_option_name = $_POST['vendor-type'] . '-' . $_POST['id'];}
 		if ( isset($_POST['delete']) ) {
 			unset($analytics_options[$inner_option_name]);
 		} else {
 			$analytics_options[$inner_option_name] = $new_analytics_options;
 		}
+
 		update_option( $option_name, $analytics_options, false);
 
 		// [Redirect] Keep the user in the analytics options page
